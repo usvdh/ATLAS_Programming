c1de1fe115380abc425dca1a8cd0f00216e7549f
Lis <l.i.debode@student.utwente.nl>
2023-03-23 16:00:15 +0100

    Add comments

--- searchengine.py
+++ searchengine.py
@@ -1,30 +1,37 @@
+# Import some programs (customtkinter is needed for the interface, os is needed to let the operating system run smoothly)
 import customtkinter
 import os
 
+# We establish the root widget, which is a window with a title bar and other decoration provided by the window manager. This way, we initialize the customtkinter.
+# We give the root a size (this case 500x500 pixels)
 root = customtkinter.CTk()
 root.geometry("500x500")
 
-
 def search():
     print("Test")
+    # print query, so it says "Search query"
     query = queryEntry.get()
     print(query)
-    # insert at line 0 character 0
+    # insert at line 0 character 0, plus the text you will see
     resultsTextbox.insert("0.0", "testfile.txt: " + "...Hello, this is a test.")
-    
+    # print the number of files there has been searched in
     print(countFiles())
 
+    # Open all files and read all lines in the file
     lines = open("textfiles/bear.txt", "r").readlines()
     for line in lines:
+        # Check if the search query is present on a line
         if line.find(query) != -1:
             print(query, 'is found')
             print('Line Number:', lines.index(line))
             print('Line:', line)
 
+#define number of files searched in
 def countFiles():
     fileList = []
     fileCounter = -1 #to match index of array
     
+    #For everything found in the system (this case only files), if it ends with '.txt', count it as 1 file
     for root, dirs, files in os.walk("textfiles"):
         for file in files:
             if file.endswith('.txt'):
@@ -34,23 +41,29 @@ def countFiles():
                 # print(fileList)
     return fileList, fileCounter
 
-
+# Establish how the interface will look like
+# Create frame, define what it looks like
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(padx=60, pady=20, fill="both")
 
+# Create text "Soogle", define what it looks like
 soogle = customtkinter.CTkLabel(master=frame, text="Soogle")
 soogle.pack(padx=10, pady=12)
 
+# Create the query entry, define what it looks like
 queryEntry = customtkinter.CTkEntry(
     master=frame, placeholder_text="Search query")
 queryEntry.pack(padx=10, pady=12)
 
+# Create the button, define what it looks like
 button = customtkinter.CTkButton(master=frame, text="Search", command=search)
 button.pack(padx=10, pady=12)
 
+# Create text "results", define what it looks like
 resultsLabel = customtkinter.CTkLabel(master=frame, text="Results")
 resultsLabel.pack(padx=10, pady=12)
 
+# Create textbox (where findings will be displayed), define what it looks like
 resultsTextbox = customtkinter.CTkTextbox(master=frame)
 resultsTextbox.pack(padx=10, pady=12)
 
