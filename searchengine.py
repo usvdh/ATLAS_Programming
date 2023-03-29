2f22981c0e23dbd9ce0a18e10ff309c3b2cefbb4
Sen van der Heide <59284496+usvdh@users.noreply.github.com>
2023-03-29 10:06:19 +0200

    Add docstrings and edit comments

--- searchengine.py
+++ searchengine.py
@@ -1,28 +1,46 @@
-# Import some programs (customtkinter is needed for the interface, os is needed to let the operating system run smoothly)
+# Read README before running for pre-requisites.
+
+# Import libraries. customtkinter is used for the graphical user interface. os is used to detect the textfiles we want to search.
 import customtkinter
 import os
 
 # We establish the root widget, which is a window with a title bar and other decoration provided by the window manager. This way, we initialize the customtkinter.
-# We give the root a size (this case 500x500 pixels)
+# We give the root a size (this case 1000x500 pixels)
 root = customtkinter.CTk()
 root.geometry("1000x500")
 
 def search():
+    '''
+    This function loops through all the files given by detectFiles() and finds the query input into the searchbox. 
+    
+    :param query: This is the query that was input into the searchbox in the GUI. 
+    :param lines: This stores all the lines seperately in an array. 
+    :return: Returns nothing, but inserts the search results to GUI textbox. 
+    '''
+    
     query = queryEntry.get()
 
     # Open all files and read all lines in the file
     for i in detectFiles(): 
         lines = open("textfiles/" + i, "r").readlines()
+        print(lines)
         for line in lines: 
             # Check if the search query is present on a line
+            # if find() finds no match, it returns -1, which is why "!= -1"
             if line.find(query) != -1:
                 resultsTextbox.insert("0.0", str(i) + ", Line " + str(lines.index(line)) + ": " + "'" + line + "'" + "\n\n")
+    return 
 
-#define number of files searched in
 def detectFiles():
+    '''
+    This function detects all .txt files in the folder 'textfiles'
+    
+    :param fileList: Array to store each text file name in.
+    :return: Returns complete array with all .txt files in the 'textfiles' folder. 
+    '''
+    
     fileList = []
     
-    #For everything found in the system (this case only files), if it ends with '.txt', count it as 1 file
     # For some reason it won't work when removing root and dirs, so still left in there.
     for root, dirs, files in os.walk("textfiles"):
         for file in files:
